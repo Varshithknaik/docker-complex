f3c843dae5687a2573f996e8ddbfb8e6cc49f7f1
Varshith K <varshithknaik@gmail.com>
2026-03-23 22:24:42 +0530

    redis setup

---
+++ worker/index.js
@@ -0,0 +1,15 @@
+const keys = require('./keys.js');
+const redis = require('redis');
+
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000,
+});
+
+const redisPublisher = redisClient.duplicate();
+
+module.exports = {
+  redisClient,
+  redisPublisher,
+};
