7c5c91c4328426f986a4abbdb2454e41d4464401
Varshith K <varshithknaik@gmail.com>
2026-03-26 23:17:09 +0530

    setup redis

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const keys = require('./keys');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const redis = require('redis');
 
 
 const app = express();
@@ -30,7 +31,41 @@ pgClient.on('connect', (client) => {
 
 pgClient.on('error', () => console.log('Lost PG connection'));
 
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
+})
 
-app.listen('5000', () => {
+const redisPublisher = redisClient.duplicate();
+
+app.get('/', (req, res) => {
+    res.send('Hi');
+})
+
+app.get('/values/all', async (req, res) => {
+    const values = await pgClient.query('SELECT * from values');
+    res.send(values.rows);
+})
+
+app.get('/values/current', async (req, res) => {
+    redisClient.hgetall('values', (err, values) => {
+        res.send(values);
+    })
+})
+
+app.post('/values', async (req, res) => {
+    const index = req.body.index;
+        if (parseInt(index) > 40) {
+            return res.status(422).send('Index too high');
+        }
+        redisClient.hset('values', index, 'Nothing yet!');
+        redisPublisher.publish('insert', index);
+        pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+        res.send({ working: true });
+})
+
+
+app.listen(5000, () => {
     console.log('Listening on port 5000');
 }) 
