a513d3410401abf16e0a22e66ed334fd9ef6026f
Varshith K <varshithknaik@gmail.com>
2026-04-07 14:04:54 +0530

    jest fix 2

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,12 +1,10 @@
 import { render, screen } from '@testing-library/react'
 import App from './App'
-import axios from 'axios'
 
 jest.mock('axios')
 
 test('renders learn react link', () => {
   render(<App />)
-  axios.get.mockResolvedValue({ data: [] })
   const linkElement = screen.getByText(/learn react/i)
   expect(linkElement).toBeInTheDocument()
 })
