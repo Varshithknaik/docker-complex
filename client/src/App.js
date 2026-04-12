675f0dbfc095dd6fcbd3d4fd2cde1d81e38d391d
Varshith K <varshithknaik@gmail.com>
2026-04-12 11:54:38 +0530

    chore: update link text from Learn React 1 to Explore the React Docs

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React 1
+            Explore the React Docs
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
