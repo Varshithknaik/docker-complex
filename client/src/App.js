7d6b650a5087c6c9d52fcf012f2dd0b12398ad4d
Varshith K <varshithknaik@gmail.com>
2026-04-23 19:23:19 +0530

    samples

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1>Fibonacci App</h1>
+          <h1>Fibonacci App Again</h1>
           <a
             className="App-link"
             href="https://reactjs.org"
